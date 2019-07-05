3d141e580d65ae424f13b21c8ff27723c921015e
dpk89 <deepak@softuvo.com>
2019-07-05 15:25:26 +0530

    update

--- db/TransactionSurvey.js
+++ db/TransactionSurvey.js
@@ -2,7 +2,18 @@ var mongoose = require('mongoose');
 
 var TransactionSurvey = new mongoose.Schema({
     Question: String,
-    Options: []
+    Option1:{
+        Option1Type: String,
+        OptionValue: []
+    },
+    Option2:{
+        Option2Type: String,
+        OptionValue: []
+    },
+    Option3:{
+        Option3Type: String,
+        OptionValue: []
+    }
 })
 
 module.exports = mongoose.model('TransactionSurvey', TransactionSurvey)
