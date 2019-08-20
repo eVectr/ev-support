93f6ff08b9b75c5a7bac16847c1f542ad3ca5002
dpk89 <deepak@softuvo.com>
2019-08-20 15:40:33 +0530

    update

--- server/server.js
+++ server/server.js
@@ -870,7 +870,7 @@ app.post('/chatnotification', (req, res) => {
   var notification = new Notification({
     Type: Type,
     Date: date,
-    SentBy: SentBy[0],
+    SentBy: SentBy,
     SentTo: SentTo,
     Action: 'SEE MESSAGE',
     FontStyle: false,
