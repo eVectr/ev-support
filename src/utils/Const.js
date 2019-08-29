2fc517ff6634e309b4b8c983aad064822b224359
dpk89 <deepak@softuvo.com>
2019-08-29 18:15:38 +0530

    update

--- src/utils/Const.js
+++ src/utils/Const.js
@@ -1,5 +1,5 @@
  const api_url = process.env.NODE_ENV === 'development'
-?   'http://localhost:7788/' : 'https://ev2.softuvo.xyz'
+?   'http://localhost:7788' : 'https://ev2.softuvo.xyz'
 
 
 export default api_url
